2e7c8408059ecdea5a707e6287885f331880b08e
placeparks <86215469+placeparks@users.noreply.github.com>
2025-12-02 00:55:05 +0500

    Update PackFactory.ts

--- app/abis/PackFactory.ts
+++ app/abis/PackFactory.ts
@@ -1,95 +1,496 @@
 export const PACK_FACTORY_ABI = [
   {
-    "type": "function",
-    "name": "createCollection",
-    "stateMutability": "nonpayable",
     "inputs": [
       {
-        "name": "p",
-        "type": "tuple",
-        "components": [
-          { "name": "name", "type": "string" },
-          { "name": "symbol", "type": "string" },
-          { "name": "owner", "type": "address" },
-          { "name": "tokenURIs", "type": "string[6]" },
-          { "name": "packPrice", "type": "uint256" },
-          { "name": "maxPacks", "type": "uint256" },
-          { "name": "royaltyBps", "type": "uint96" },
-          { "name": "royaltyReceiver", "type": "address" },
-          { "name": "marketplace", "type": "address" },
-          { "name": "initialMintTo", "type": "address" },
-          { "name": "initialMintAmount", "type": "uint256" }
-        ]
+        "internalType": "address",
+        "name": "_implementation",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_feeReceiver",
+        "type": "address"
+      },
+      {
+        "internalType": "uint96",
+        "name": "_feeBps",
+        "type": "uint96"
+      },
+      {
+        "internalType": "address",
+        "name": "_defaultMarketplace",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_owner",
+        "type": "address"
       }
     ],
-    "outputs": [{ "name": "col", "type": "address" }]
-  },
-  {
-    "type": "function",
-    "name": "setDefaults",
     "stateMutability": "nonpayable",
-    "inputs": [
-      { "name": "_feeReceiver", "type": "address" },
-      { "name": "_feeBps", "type": "uint96" },
-      { "name": "_marketplace", "type": "address" }
-    ],
-    "outputs": []
+    "type": "constructor"
   },
   {
-    "type": "function",
-    "name": "registerExistingCollection",
-    "stateMutability": "nonpayable",
-    "inputs": [
-      { "name": "collection", "type": "address" },
-      { "name": "ownerHint", "type": "address" }
-    ],
-    "outputs": []
-  },
-  {
-    "type": "function",
-    "name": "isCardifyCollection",
-    "stateMutability": "view",
-    "inputs": [{ "name": "col", "type": "address" }],
-    "outputs": [{ "type": "bool" }]
-  },
-  {
-    "type": "function",
-    "name": "totalCollections",
-    "stateMutability": "view",
     "inputs": [],
-    "outputs": [{ "type": "uint256" }]
+    "name": "FailedDeployment",
+    "type": "error"
   },
   {
-    "type": "function",
-    "name": "allCollections",
-    "stateMutability": "view",
-    "inputs": [{ "name": "index", "type": "uint256" }],
-    "outputs": [{ "type": "address" }]
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "balance",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "needed",
+        "type": "uint256"
+      }
+    ],
+    "name": "InsufficientBalance",
+    "type": "error"
   },
   {
-    "type": "function",
-    "name": "collectionsOf",
-    "stateMutability": "view",
-    "inputs": [{ "name": "user", "type": "address" }],
-    "outputs": [{ "type": "address[]" }]
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnableInvalidOwner",
+    "type": "error"
   },
   {
-    "type": "event",
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
+    ],
+    "name": "OwnableUnauthorizedAccount",
+    "type": "error"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "creator",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "collection",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
     "name": "CollectionCreated",
-    "inputs": [
-      { "name": "creator", "type": "address", "indexed": true },
-      { "name": "collection", "type": "address", "indexed": true },
-      { "name": "owner", "type": "address", "indexed": true }
-    ],
-    "anonymous": false
+    "type": "event"
   },
   {
-    "type": "event",
-    "name": "CollectionRegistered",
+    "anonymous": false,
     "inputs": [
-      { "name": "collection", "type": "address", "indexed": true },
-      { "name": "owner", "type": "address", "indexed": true }
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "collection",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
     ],
-    "anonymous": false
+    "name": "CollectionRegistered",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "feeReceiver",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint96",
+        "name": "feeBps",
+        "type": "uint96"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "marketplace",
+        "type": "address"
+      }
+    ],
+    "name": "DefaultsUpdated",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "impl",
+        "type": "address"
+      }
+    ],
+    "name": "ImplementationSet",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "allCollections",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "byCreator",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      }
+    ],
+    "name": "collectionsOf",
+    "outputs": [
+      {
+        "internalType": "address[]",
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "internalType": "string",
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "symbol",
+            "type": "string"
+          },
+          {
+            "internalType": "address",
+            "name": "owner",
+            "type": "address"
+          },
+          {
+            "internalType": "string[]",
+            "name": "tokenURIs",
+            "type": "string[]"
+          },
+          {
+            "internalType": "uint256",
+            "name": "packPrice",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "maxPacks",
+            "type": "uint256"
+          },
+          {
+            "internalType": "address",
+            "name": "royaltyReceiver",
+            "type": "address"
+          },
+          {
+            "internalType": "uint96",
+            "name": "royaltyBps",
+            "type": "uint96"
+          },
+          {
+            "internalType": "address",
+            "name": "feeReceiver",
+            "type": "address"
+          },
+          {
+            "internalType": "uint96",
+            "name": "feeBps",
+            "type": "uint96"
+          },
+          {
+            "internalType": "address",
+            "name": "marketplace",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "initialMintTo",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "initialMintAmount",
+            "type": "uint256"
+          }
+        ],
+        "internalType": "struct PackFactory.CreateParams",
+        "name": "p",
+        "type": "tuple"
+      }
+    ],
+    "name": "createCollection",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "col",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "defaultMarketplace",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "feeBps",
+    "outputs": [
+      {
+        "internalType": "uint96",
+        "name": "",
+        "type": "uint96"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "feeReceiver",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "implementation",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "col",
+        "type": "address"
+      }
+    ],
+    "name": "isCardifyCollection",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "col",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "colOwner",
+        "type": "address"
+      }
+    ],
+    "name": "registerExistingCollection",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_feeReceiver",
+        "type": "address"
+      },
+      {
+        "internalType": "uint96",
+        "name": "_feeBps",
+        "type": "uint96"
+      },
+      {
+        "internalType": "address",
+        "name": "_defaultMarketplace",
+        "type": "address"
+      }
+    ],
+    "name": "setDefaults",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "impl",
+        "type": "address"
+      }
+    ],
+    "name": "setImplementation",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalCollections",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   }
 ] as const
